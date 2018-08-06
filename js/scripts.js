8ab7fe8bd6712fcf257c321130fca5a1dc6f7312
Colin Cook and Eric Conner <eric@>
2018-08-06 16:46:16 -0700

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,22 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+}
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}
 
-
-
+var multiply = function(number1, number2) {
+  return number1 * number2;
+}
 
 var divide = function(number1, number2) {
-	return number1 / number2;
+  return number1 / number2;
 }
 
-var result = divide(10,2);
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
 
-alert(result);
-
-var age = prompt("Enter your age: ");
-
-var name = prompt("Enter your name: ");
-
-var favoriteFood = prompt("Enter your favorite food: ");
-
-var add = function(age, name, favoriteFood) {
-	return age + name + favoriteFood;
-}
-
-var result = add("I'm " + age + " years old. ", "My name is " + name + ". ", "My favorite food is " + favoriteFood + ".");
+var result = divide(number1, number2);
 
 alert(result);
