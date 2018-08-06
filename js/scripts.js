5df53ee33e988d600e34e6cf00bb6aef45bc9753
Colin Cook and Eric Conner <colin@>
2018-08-06 11:20:00 -0700

    add html, js, readme.md

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+}
+
+var result = divide(10,2);
+
+alert(result);
+
+var age = prompt("Enter your age: ");
+
+var name = prompt("Enter your name: ");
+
+var favoriteFood = prompt("Enter your favorite food: ");
+
+var add = function(age, name, favoriteFood) {
+	return age + name + favoriteFood;
+}
+
+var result = add("I'm " + age + " years old. ", "My name is " + name + ". ", "My favorite food is " + favoriteFood + ".");
+
+alert(result);
